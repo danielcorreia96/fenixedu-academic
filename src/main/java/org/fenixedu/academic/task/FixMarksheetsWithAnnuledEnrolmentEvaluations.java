422dd7f813ced7d95e037a9461c9b620e00879ac
Daniel Correia <dduarte96@hotmail.com>
2017-11-29 12:32:17 +0000

    added deletion mechanisms to script.
    TODO: wait for validation from colleagues

--- src/main/java/org/fenixedu/academic/task/FixMarksheetsWithAnnuledEnrolmentEvaluations.java
+++ src/main/java/org/fenixedu/academic/task/FixMarksheetsWithAnnuledEnrolmentEvaluations.java
@@ -6,17 +6,13 @@ import org.fenixedu.academic.domain.ExecutionSemester;
 import org.fenixedu.academic.domain.MarkSheet;
 import org.fenixedu.academic.util.EnrolmentEvaluationState;
 import org.fenixedu.bennu.scheduler.custom.CustomTask;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class FixMarksheetsWithAnnuledEnrolmentEvaluations extends CustomTask {
-
-    private static final Logger logger = LoggerFactory.getLogger(FixMarksheetsWithAnnuledEnrolmentEvaluations.class);
 
     @Override
     public void runTask() throws Exception {
         final ExecutionSemester semester = ExecutionSemester.readActualExecutionSemester();
-        logger.info("Checking marksheets for " + semester.getQualifiedName());
+        taskLog("Checking marksheets for %s\n", semester.getQualifiedName());
         semester.getAssociatedExecutionCoursesSet().forEach(this::validate);
     }
 
@@ -25,18 +21,21 @@ public class FixMarksheetsWithAnnuledEnrolmentEvaluations extends CustomTask {
     }
 
     private void validate(final MarkSheet ms) {
+        taskLog("Validating marksheet with id %s\n", ms.getExternalId());
         ms.getEnrolmentEvaluationsSet().forEach(this::validate);
-        logger.info("Validating marksheet with id " + ms.getExternalId());
+        if (ms.getEnrolmentEvaluationsSet().isEmpty()) {
+            taskLog("Marksheet became empty after validation...deleting it");
+            ms.delete();
+        }
     }
 
     private void validate(final EnrolmentEvaluation ee) {
         if (ee.getEnrolmentEvaluationState().equals(EnrolmentEvaluationState.ANNULED_OBJ)) {
-            logger.error("Found a student with annulled enrolment evaluation - id: " + ee.getExternalId());
-            logger.error("Current grade: " + ee.getGradeValue());
-            if(ee.getSpecialSeasonEnrolmentEvent() != null){
-                logger.error("Special Season Event with id: " + ee.getSpecialSeasonEnrolmentEvent().getExternalId());
-            }
+            taskLog("\tFound a student with annulled enrolment evaluation - id: %s\n", ee.getExternalId());
+            taskLog("\tCurrent grade: %s\n", ee.getGradeValue());
+            taskLog("\tStudent number: %d\n", ee.getStudentCurricularPlan().getStudent().getStudent().getStudentNumber().getNumber());
+            ee.removeFromMarkSheet();
+            taskLog("Removed student from marksheet");
         }
     }
-
 }
